58ca47b034b0412529b9fde8df46d16d7bc471bf
Thomas Labbé <carrecuir@gmail.com>
2019-09-21 13:51:15 -0400

    Ajout de la commande en ligne droite

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,49 @@
 #include <Arduino.h>
+#include <../lib/LibRobUS/src/LibRobus.h>
+float speed_G;
+float speed_D;
 
 void setup() {
-  // put your setup code here, to run once:
+  BoardInit();
+  speed_G = 0.5;
+  speed_D = 0.5;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Pulse par ms
+  /*if(ROBUS_IsBumper(3)){
+    MOTOR_SetSpeed(1, 1);
+    MOTOR_SetSpeed(0, 1);
+  
+    delay(1000);
+    for(int i = 0; i < 50; i++){
+      delay(100);
+      Serial.println(ENCODER_Read(0));
+      //Serial.println(",")
+    }
+    Serial.print("*************************************************************");
+    for(int i = 0; i < 50; i++){
+      delay(100);
+      Serial.println(ENCODER_Read(1));
+    }
+    MOTOR_SetSpeed(0, 0);
+    MOTOR_SetSpeed(1, 0);
+    
+  }*/
+  
+  int encoder_G, encoder_D;
+  if(ROBUS_IsBumper(3)){
+    while(!ROBUS_IsBumper(2) and !ROBUS_IsBumper(1) and !ROBUS_IsBumper(0)){
+        encoder_G = ENCODER_ReadReset(0);
+        encoder_D = ENCODER_ReadReset(1);
+        MOTOR_SetSpeed(1, speed_G);
+        MOTOR_SetSpeed(0, speed_D);
+        speed_G = speed_G + 0.00015*(encoder_D - encoder_G);
+        delay(100);
+      }
+
+  }
+  MOTOR_SetSpeed(1, 0);
+  MOTOR_SetSpeed(0, 0);
 }
