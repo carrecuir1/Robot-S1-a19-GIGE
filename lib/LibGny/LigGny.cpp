8986a269ed18add2cac39d35139c0bd40fe57c7b
Emile Chartray <chae3211@usherbrooke.ca>
2019-09-26 14:16:59 -0400

    Modif to variable feedback and i

--- lib/LibGny/LigGny.cpp
+++ lib/LibGny/LigGny.cpp
@@ -1,25 +1,23 @@
 #include <Arduino.h>
 #include <../lib/LibRobUS/src/LibRobus.h>
 
-#define Ki 0 // intégrale
-#define Kp 0 
-#define Kd 0 // dériver
-#define Periode 200 
 float pid(int consigne,int feedbackPresent,  int feedbackPasse); // consigne = v mit, feedback = v réel
 float iPresent = 0;  // pas touche
+int feedbackPasse = 0;
 
 #pragma region PID
-float pid(int consigne,int feedbackPresent, int feedbackPasse)
+float pid(int consigne)
 {
+    const float ki = 1, kd = 1, kp = 1;
+    float iPasse;
+    int feedbackPresent = ENCODER_ReadReset(0);
+    int erreur = consigne - feedbackPresent;
+    float p = kp * erreur ;
+    iPasse = iPresent;
+    iPresent = (ki *  erreur ) + iPasse;
+    float d = kd * ( feedbackPresent - feedbackPasse);
+    float commande = p +iPresent - d ;
 
-  float iPasse; 
-  int erreur = consigne - feedbackPresent;
-  float p = Kp * erreur ;
-  iPasse = iPresent; 
-  float iPresent = Ki * (( erreur * Periode ) + iPasse);
-  float d = Kd * (( feedbackPresent - feedbackPasse)/Periode);
-  float commande = p +iPresent - d ;
-
-  return commande;  
+    return commande;  
 }
 #pragma endregion
