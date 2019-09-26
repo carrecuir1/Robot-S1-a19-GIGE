ce9a72ab2d0d069f0fea5a2819ff443f1634b157
Émile Chartray <chae3211@usherbrooke.ca>
2019-09-26 13:18:57 -0400

    create function pid

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,26 @@
 #include <../lib/LibRobUS/src/LibRobus.h>
 float speed_G;
 float speed_D;
+#define Ki 0
+#define Kp 0
+#define Kd 0
+#define Periode 200 
+float pid(int consigne,int feedback); // consigne = v mit, feedback = v réel
+float iPresent = 0;  // pas touche
 
 void setup() {
   BoardInit();
   speed_G = 0.5;
   speed_D = 0.5;
+  
 }
 
 void loop() {
+
+
+
+
+
 
   //Pulse par ms
   /*if(ROBUS_IsBumper(3)){
@@ -46,4 +58,19 @@ void loop() {
   }
   MOTOR_SetSpeed(1, 0);
   MOTOR_SetSpeed(0, 0);
+}
+
+
+float pid(int consigne,int feedbackPresent, int feedbackPasse)
+{
+
+  float iPasse; 
+  int erreur = consigne - feedbackPresent;
+  float p = Kp * erreur ;
+  iPasse = iPresent; 
+  float iPresent = Ki * (( erreur * Periode ) + iPasse);
+  float d = Kd * (( feedbackPresent - feedbackPasse)/Periode);
+  float commande = p +iPresent - d ;
+
+  return commande;  
 }
