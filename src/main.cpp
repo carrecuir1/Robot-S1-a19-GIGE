44ee68bf12507847c7b474e668b7df354087e533
Maxence Cote <maxencecote08@hotmail.com>
2019-10-03 09:14:31 -0400

    Ajout de la fonction avancer sans le PID

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include <../lib/LibRobUS/src/LibRobus.h>
+#include <LibGny.h>
+#include <LibRobus.h>
 float speed_G;
 float speed_D;
 
@@ -11,39 +12,10 @@ void setup() {
 
 void loop() {
 
-  //Pulse par ms
-  /*if(ROBUS_IsBumper(3)){
-    MOTOR_SetSpeed(1, 1);
-    MOTOR_SetSpeed(0, 1);
-  
-    delay(1000);
-    for(int i = 0; i < 50; i++){
-      delay(100);
-      Serial.println(ENCODER_Read(0));
-      //Serial.println(",")
-    }
-    Serial.print("*************************************************************");
-    for(int i = 0; i < 50; i++){
-      delay(100);
-      Serial.println(ENCODER_Read(1));
-    }
-    MOTOR_SetSpeed(0, 0);
-    MOTOR_SetSpeed(1, 0);
-    
-  }*/
-  
-  int encoder_G, encoder_D;
-  if(ROBUS_IsBumper(3)){
-    while(!ROBUS_IsBumper(2) and !ROBUS_IsBumper(1) and !ROBUS_IsBumper(0)){
-        encoder_G = ENCODER_ReadReset(0);
-        encoder_D = ENCODER_ReadReset(1);
-        MOTOR_SetSpeed(1, speed_G);
-        MOTOR_SetSpeed(0, speed_D);
-        speed_G = speed_G + 0.00015*(encoder_D - encoder_G);
-        delay(100);
-      }
-
+  if(ROBUS_IsBumper(3))
+  {
+    Serial.println("test");
+    avancerRobot(100);
+    delay(100);
   }
-  MOTOR_SetSpeed(1, 0);
-  MOTOR_SetSpeed(0, 0);
 }
