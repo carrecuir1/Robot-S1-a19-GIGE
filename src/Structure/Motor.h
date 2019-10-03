c5ef15019bed6d89a5a78a5e2d84d259338a1b16
Maxence Cote <maxencecote08@hotmail.com>
2019-10-03 12:58:27 -0400

    Ajout du parcours

--- src/Structure/Motor.h
+++ src/Structure/Motor.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <LibRobus.h>
 
-#define circumference 1171.1814059789
+#define circumference 1169.0
 #define distancePulse 0.074809175064
 
 struct Motor {
@@ -13,7 +13,7 @@ struct Motor {
     void angleTurn(float angle, float consigne){
         int selectEncodeur = 0;
         int32_t encoder = 0;
-        float pulseToAchieve = (angle*circumference)/(360*distancePulse);
+        float pulseToAchieve = (abs(angle)*circumference)/(360*distancePulse);
 
         if(angle < 0){
             selectEncodeur = 1;
@@ -22,7 +22,7 @@ struct Motor {
         ENCODER_Reset(selectEncodeur);
         MOTOR_SetSpeed(selectEncodeur,consigne);
         
-        while(encoder < pulseToAchieve){
+        while(abs(encoder) < pulseToAchieve){
             encoder = ENCODER_Read(selectEncodeur);
         }
         MOTOR_SetSpeed(selectEncodeur,0);
