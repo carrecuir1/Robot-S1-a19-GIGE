af98e066e101f9751927b403cc1615804c982529
Maxence Cote <maxencecote08@hotmail.com>
2019-10-08 12:24:18 -0400

    Finalisation parcours et ajustement de la fonction tourner

--- src/Structure/Motor.h
+++ src/Structure/Motor.h
@@ -2,27 +2,29 @@
 #include <LibRobus.h>
 
 //chiffre doit terminer par ".0"
-#define circumference 1169.0 
+#define circumference 1168.672467
 #define distancePulse 0.074809175064
 #define diameter 23.94
 
 struct Motor {
     
-    Motor(){
-        
+    float speed;
+
+    Motor(float consigne){
+        speed = consigne;
     }
     
-    void angleTurn(float angle, float consigne){
+    void angleTurn(float angle){
         int selectEncodeur = 0;
         int32_t encoder = 0;
-        float pulseToAchieve = (abs(angle)*circumference)/(360*distancePulse);
+        float pulseToAchieve = 0.95*((abs(angle)*circumference)/(360*distancePulse));
 
         if(angle < 0){
             selectEncodeur = 1;
         }
         
         ENCODER_Reset(selectEncodeur);
-        MOTOR_SetSpeed(selectEncodeur,consigne);
+        MOTOR_SetSpeed(selectEncodeur, 0.2);
         
         while(abs(encoder) < pulseToAchieve){
             encoder = ENCODER_Read(selectEncodeur);
@@ -32,16 +34,16 @@ struct Motor {
         Serial.println(pulseToAchieve);
     }
 
-    void demiTour(float consigne)
+    void uTurn()
     {
-        PID pid = PID(consigne);
+        PID pid = PID(speed);
 
         float pulseToAchieve = (180*circumference)/(360*distancePulse);
         int32_t encoderL = 0, encoderR = 0;
         ENCODER_Reset(0);
         ENCODER_Reset(1);
 
-        MOTOR_SetSpeed(0,consigne);
+        MOTOR_SetSpeed(0, speed);
         MOTOR_SetSpeed(1,-1*pid.getPID());
 
         //while(encoderL < pulseToAchieve && -1*encoderR < pulseToAchieve)
@@ -54,46 +56,21 @@ struct Motor {
         MOTOR_SetSpeed(1, 0);
 
     }
-    
-
    
-   
-    void straightRun(float distance, float consigne)
+    void straightRun(float distance)
     {
         float distanceRight = 0;
-        PID motor(consigne);
+        PID motor(speed);
         ENCODER_Reset(0);
         ENCODER_Reset(1);
         do
         {
-           /*if(distanceRight > (0.7*distance))
-            {
-                if(distanceRight > (0.9*distance))
-                {
-                    MOTOR_SetSpeed(0.2, consigne);
-                    MOTOR_SetSpeed(0, motor.getPID());
-                    distanceRight = (diameter * ENCODER_Read(1))/3200;
+            Serial.println(speed);
+            Serial.println(motor.getPID());
 
-                    Serial.println("ralenti 2");
-                }
-                else
-                {          
-                    MOTOR_SetSpeed(1, 0.3);
-                    MOTOR_SetSpeed(0, motor.getPID());
-                    distanceRight = (diameter * ENCODER_Read(1))/3200;
-
-                    Serial.println("ralenti");
-                } 
-            }
-            else*/
-           // {
-                Serial.println(consigne);
-                Serial.println(motor.getPID());
-
-                MOTOR_SetSpeed(1, consigne);
-                MOTOR_SetSpeed(0, motor.getPID());
-                distanceRight = (diameter * ENCODER_Read(1))/3200;
-           // }
+            MOTOR_SetSpeed(1, speed);
+            MOTOR_SetSpeed(0, motor.getPID());
+            distanceRight = (diameter * ENCODER_Read(1))/3200;
             
         }while(distanceRight < distance);
         Serial.println(distanceRight);
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,8 @@
 #include <Structure/PID.h>
 #include <Structure/Motor.h>
 
-<<<<<<< HEAD
-#define consigne 0.3
-bool stop = false;
-=======
 #define consigne 0.35
 bool test;
->>>>>>> c20fe3d3e59b316f5c4173779bbfb8312d457cd2
 void setup() {
   BoardInit();
   test = true;
@@ -18,7 +13,7 @@ void setup() {
 void loop() {
 
   PID pid = PID(consigne);
-  Motor motor = Motor();
+  Motor motor = Motor(consigne);
   /*if(ROBUS_IsBumper(0)){
     motor.demiTour(consigne);
   }*/
@@ -26,72 +21,57 @@ void loop() {
   if(ROBUS_IsBumper(3)){
     while(!ROBUS_IsBumper(2) and !ROBUS_IsBumper(1) and !ROBUS_IsBumper(0) and test == true){
         //MOTOR_SetSpeed(1, pid.get(vitesseMoteur, 0 , 100));
-<<<<<<< HEAD
-        motor.straightRun(115, consigne);
+        motor.straightRun(122);
         delay(100);
-        motor.angleTurn(-85,consigne);
+        motor.angleTurn(-90);
         delay(100);
-        motor.straightRun(80, consigne);
+        motor.straightRun(70);
         delay(100);
-=======
-        motor.straightRun(122, consigne);
-        delay(10);
-        motor.angleTurn(-90,consigne);
-        delay(10);
-        motor.straightRun(75, consigne);
-        delay(10);
->>>>>>> c20fe3d3e59b316f5c4173779bbfb8312d457cd2
-        motor.angleTurn(90,consigne);
-        delay(10);
-        motor.straightRun(65, consigne);
-        delay(10);
-        motor.angleTurn(45,consigne);
-<<<<<<< HEAD
+        motor.angleTurn(90);
         delay(100);
-        motor.straightRun(169, consigne);
+        motor.straightRun(70);
         delay(100);
-=======
-        delay(10);
-        motor.straightRun(180, 0.5);
-        delay(10);
->>>>>>> c20fe3d3e59b316f5c4173779bbfb8312d457cd2
-        motor.angleTurn(-90,consigne);
-        delay(10);
-        motor.straightRun(50, consigne);
-        delay(10);
-        motor.angleTurn(45,consigne);
-        delay(10);
-        motor.straightRun(120, consigne);
-        delay(10);
+        motor.angleTurn(45);
+        delay(100);
+        motor.straightRun(175);
+        delay(100);
+        motor.angleTurn(-90);
+        delay(100);
+        motor.straightRun(50);
+        delay(100);
+        motor.angleTurn(45);
+        delay(100);
+        motor.straightRun(120);
+        delay(100);
         //MILIEUX 
-        motor.demiTour(consigne);
+        motor.uTurn();
         delay(100);
-        motor.straightRun(120, consigne);
+        motor.straightRun(120);
         delay(100);
-        motor.angleTurn(-45, consigne);
+        motor.angleTurn(-45);
         delay(100);
-        motor.straightRun(60, consigne);
+        motor.straightRun(50);
         delay(100);
-        motor.angleTurn(90,consigne);
+        motor.angleTurn(90);
         delay(100);
-        motor.straightRun(185, 0.5);
+        motor.straightRun(175);
         delay(100);
-        motor.angleTurn(-45, consigne);
+        motor.angleTurn(-45);
         delay(100);
-        motor.straightRun(72, consigne);
+        motor.straightRun(70);
         delay(100);
-        motor.angleTurn(-90,consigne);
+        motor.angleTurn(-90);
         delay(100);
-        motor.straightRun(75, consigne);
+        motor.straightRun(70);
         delay(100);
-        motor.angleTurn(90,consigne);
+        motor.angleTurn(90);
         delay(100);
-        motor.straightRun(125, consigne);
+        motor.straightRun(122);
         delay(100);
-        motor.demiTour(consigne);
-        motor.demiTour(consigne);
-        motor.demiTour(consigne);
-        motor.demiTour(consigne);
+        motor.uTurn();
+        motor.uTurn();
+        motor.uTurn();
+        motor.uTurn();
         test = false;
     }
   }
