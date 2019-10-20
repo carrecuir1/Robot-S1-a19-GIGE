723c07086a82f2c565909edc8b241bc641589d54
Martin Giroux <girm1002@usherbrooke.ca>
2019-10-20 12:41:32 -0400

    regle potentielles erreurs

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,17 @@ void setup() {
 }
 
 void loop() {
-
- if(ROBUS_IsBumper(3)){
     float voltage0, voltage1, voltage2;
+    int sensorValue0, sensorValue1, sensorValue2;
+  if(ROBUS_IsBumper(3)){
     Serial.begin(9600);
-    int sensorValue0 = analogRead(0), sensorValue1 = analogRead(1), sensorValue2 = analogRead(2);
+    sensorValue0 = analogRead(0);
+    sensorValue1 = analogRead(1);
+    sensorValue2 = analogRead(2);
     voltage0 = sensorValue0 * 5.0/1023.0;
     voltage1 = sensorValue1 * 5.0/1023.0;
     voltage2 = sensorValue2 * 5.0/1023.0;
     Serial.println("Voltage 1 : " + voltage0);
     Serial.println("Voltage 2 : " + voltage1);
     Serial.println("Voltage 3 : " + voltage2);
-  }
 }
