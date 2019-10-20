b3e1ba3eb63f488e8fd281807d93a500255e4047
Martin Giroux <girm1002@usherbrooke.ca>
2019-10-19 20:31:03 -0400

    Test code for line follower captor

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,15 @@ void setup() {
 
 void loop() {
 
-  Motor motor = Motor(consigne);
-
-  if(ROBUS_IsBumper(3)){
-    while(!ROBUS_IsBumper(2) and !ROBUS_IsBumper(1) and !ROBUS_IsBumper(0) and test == true){
-
-        test = false;
-    }
+ if(ROBUS_IsBumper(3)){
+    float voltage0, voltage1, voltage2;
+    Serial.begin(9600);
+    int sensorValue0 = analogRead(0), sensorValue1 = analogRead(1), sensorValue2 = analogRead(2);
+    voltage0 = sensorValue0 * 5.0/1023.0;
+    voltage1 = sensorValue1 * 5.0/1023.0;
+    voltage2 = sensorValue2 * 5.0/1023.0;
+    Serial.println("Voltage 1 : " + voltage0);
+    Serial.println("Voltage 2 : " + voltage1);
+    Serial.println("Voltage 3 : " + voltage2);
   }
 }
