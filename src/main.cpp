c06343d695e02a14d3057211a403daa49ba0bc62
Martin Giroux <girm1002@usherbrooke.ca>
2019-10-29 10:55:16 -0400

    Deboguage, tests fonctionnels pour voltage analogique

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,24 @@
 bool test;
 void setup() {
   BoardInit();
-  test = true;
+  Serial.begin(9600);
 }
 
 void loop() {
     float voltage0, voltage1, voltage2;
-    int sensorValue0, sensorValue1, sensorValue2;
-  if(ROBUS_IsBumper(3)){
-    Serial.begin(9600);
+    float sensorValue0, sensorValue1, sensorValue2;
+if(ROBUS_IsBumper(3)){
     sensorValue0 = analogRead(0);
     sensorValue1 = analogRead(1);
     sensorValue2 = analogRead(2);
-    voltage0 = sensorValue0 * 5.0/1023.0;
-    voltage1 = sensorValue1 * 5.0/1023.0;
-    voltage2 = sensorValue2 * 5.0/1023.0;
+    voltage0 = sensorValue0 * 5.00/1023.00;
+    voltage1 = sensorValue1 * 5.00/1023.00;
+    voltage2 = sensorValue2 * 5.00/1023.00;
     Serial.println(voltage0);
     Serial.println(voltage1);
     Serial.println(voltage2);
+    Serial.println("-----");
+    delay(2000);
+   }
+  
 }
