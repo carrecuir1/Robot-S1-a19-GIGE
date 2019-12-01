27f7c963323f2fd6b9b44be1a8f21be3e104b3ca
Thomas Labbé <carrecuir@gmail.com>
2019-12-01 14:16:42 -0500

    Ajout de la fonction disponible

--- src/Structure/Instruction.h
+++ src/Structure/Instruction.h
@@ -3,30 +3,23 @@
 #include <Structure/Motor.h>
 #include <Structure/Servo.h>
 #include <Structure/Bluetooth.h>
-//#include <Structure/suiveurLigne.h>
 #include <Structure/capteurIR.h>
 
-struct Instruction {
 
-    typedef enum {
-        red,
-        yellow,
-        green,
-        blue
-    } color;
+//Va contenir les intrstuctions à effectuer pour le robot
+struct Instruction {
 
     Motor motor;
     Servo servo;
     Bluetooth bt;
-   // suiveurLigne suiveurligne;
-    ////
-    //capteurIR capteurir;
-    //
     float speed;
+    bool motorActive;
+
     Instruction(float consigne){
         motor = Motor();
         speed = consigne;
         bt = Bluetooth();
+        motorActive = false;
     }
 
     void getBTInsctruction(){
@@ -34,170 +27,27 @@ struct Instruction {
         switch (state)
         {
         case 0:
-            bt.sendMessage("Inactive"); // Send back, to the phone, the String "LED: ON"
+            //bt.sendMessage("Inactive"); // Send back, to the phone, the String "LED: ON"
             //desactivation des moteurs DC
             break;
         case 1:
-            bt.sendMessage("Active"); // Send back, to the phone, the String "LED: ON"
+            //bt.sendMessage("Active"); // Send back, to the phone, the String "LED: ON"
             //activation des moteurs DC
             break;
         case 2:
-            bt.sendMessage("Left"); //Tourne les moteurs à gauche
-            motor.turn(-1);
+            motor.turn(-1);//Tourne les moteurs à gauche
             delay(500);
             break;
         case 3:
-            bt.sendMessage("Right");
             motor.turn(1);
             delay(500);
+            break;
+        case 4:
+            //Tire une balle
             break;
         default:
             motor.stopMotors();
             break;
         }
     }
-    /*void testIR(){
-        float dist;
-        dist = capteurir.getDistance();
-        Serial.println(dist);
-    }*/
-    //Fonction qui va contenir les instructions du robot A
-    
-    void warriorChallengeA(){
-        color valide = blue;
-
-        switch (valide)
-        {
-        case red:
-            redA();
-            break;
-        case yellow:
-            yellowA();
-            break;
-        case green:
-            greenA();
-            break;
-        case blue:
-            blueA();
-            break;
-        }
-        servo.catchBall();
-        motor.moveDistance(10, -0.2);
-        motor.uTurn(0.2);
-        motor.angleTurn(5);
-        motor.moveDistance(86, 0.3);
-        servo.openPliers();
-        motor.moveDistance(100, -0.2);
-        motor.angleTurn(45);
-        motor.moveDistance(60, 0.3);
-        motor.angleTurn(-90);
-        motor.moveDistance(20, -0.3);
-    }
-
-    //Fonction qui va contenir les instructions du robot B
-    void warriorChallengeB(){
-        color valide = green;
-        
-        switch (valide)
-        {
-        case red:
-            redB();
-            break;
-        case yellow:
-            yellowB();
-            break;
-        case green:
-            greenB();
-            break;
-        case blue:
-            blueB();
-            break;
-        }
-        motor.moveDistance(100, 0.8);
-    }
-
-    void redA(){
-        motor.angleTurn(-90);
-        motor.moveDistance(45, 0.3);
-        motor.angleTurn(90);
-        motor.moveDistance(80, 0.3);
-        motor.angleTurn(-45);
-    }
-
-    void yellowA(){
-        motor.angleTurn(-45);
-        motor.moveDistance(11, 0.4);
-        motor.angleTurn(-97);
-    }
-
-    void greenA(){
-        motor.angleTurn(90);
-        motor.moveDistance(45, 0.3);
-        motor.angleTurn(-90);
-        motor.moveDistance(80, 0.3);
-        motor.angleTurn(45);
-    }
-    
-    void blueA(){
-        motor.moveDistance(10, -0.3);
-        /*motor.angleTurn(90);
-        motor.moveDistance(12, 0.4);
-        motor.angleTurn(95);
-        motor.moveDistance(10, 0.4);
-        motor.angleTurn(-95);*/
-        motor.angleTurn(45);
-        motor.moveDistance(11, 0.4);
-        motor.angleTurn(97);
-    }
-
-    void redB(){
-        motor.moveDistance(10, -0.3);
-        motor.angleTurn(-90);
-        motor.moveDistance(15, 0.3);
-        motor.angleTurn(90);
-        motor.moveDistance(31, 0.3);
-        motor.angleTurn(135);
-        motor.moveOverCircle(31);
-    }
-
-    void yellowB(){
-        /*motor.angleTurn(-45);
-        motor.moveDistance(12, 0.3);
-        motor.angleTurn(90);
-        motor.moveOverCircle(31);*/
-        motor.moveDistance(10, -0.3);
-        motor.angleTurn(-90);
-        motor.moveDistance(10, 0.3);
-        motor.angleTurn(135); 
-        motor.moveDistance(10, 0.3); 
-        motor.moveOverCircle(31);
-    }
-
-    void greenB(){
-        motor.moveDistance(10, -0.3);
-        motor.angleTurn(90);
-        motor.moveDistance(17, 0.3);
-        motor.angleTurn(-90);
-        motor.moveDistance(31, 0.3);
-        motor.angleTurn(-140);
-        motor.moveOverCircle(31);
-        /*motor.moveDistance(10, -0.3);
-        motor.angleTurn(-90);
-        motor.moveDistance(10, 0.3);
-        motor.angleTurn(135); 
-        motor.moveDistance(10, 0.3); 
-        motor.moveOverCircle(31);*/
-    }
-    
-    void blueB(){
-        motor.moveDistance(10, -0.3);
-        motor.angleTurn(90);
-        motor.moveDistance(10, 0.3);
-        motor.angleTurn(-135); 
-        motor.moveDistance(10, 0.3); 
-        motor.moveOverCircle(31);
-        
-    }
-
-
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
-#include <main.h>
+#include <Main.h>
 
-void setup() {
+SoftwareSerial BTSerial(17, 16); // RX | TX
+
+void setup() 
+{
+  BoardInit();
+  pinMode(9, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  digitalWrite(9, HIGH); 
+  Serial.begin(9600);
+  Serial.println("Enter AT commands:");
+  Serial.println("AT");
+  BTSerial.begin(38400);  // HC-05 default speed in AT command more
 }
 
-void loop() {
-  Serial.print("!");
-  Serial.print("Something to send.\r\n");
-  //Instruction instruction = Instruction(0.2);
-  //instruction.getBTInsctruction;
-}
+void loop()
+{
 
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+    BTSerial.write(Serial.read());
+}
